ac6b59500402ebfb5a458b16e77beaf96f1f242c
pradipdhale <pradipdhale127@gmail.com>
2023-01-14 10:09:54 +0530

    header component created

--- src/components/Partials/Header.jsx
+++ src/components/Partials/Header.jsx
@@ -1,8 +1,19 @@
+import styled from '@emotion/styled'
 import React from 'react'
+import LOGO from '../../assets/logo.png'
+
+const StyledIMG = styled('img')`
+   height: 36.02px;
+   width: 160px;
+   margin-top: 60px;
+   margin-left: 60px;
+`
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <StyledIMG src={LOGO} />
+    </>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,22 @@
+import styled from '@emotion/styled'
+import { Box } from '@mui/material'
 import React from 'react'
 import FormContainer from './Partials/FormContainer'
 import Header from './Partials/Header'
 
+
+const StyledBox = styled(Box)`
+   width: 100%;
+   height: 500px;
+//    background-color: #E5E5E5;
+`
+
 const MainBox = () => {
   return (
-    <>
+    <StyledBox>
     <Header />
     <FormContainer />
-    </>
+    </StyledBox>
   )
 }
 
